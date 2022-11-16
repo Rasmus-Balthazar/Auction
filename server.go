c4bcf5a43dc6913eace5829d7523b1029801c440
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2022-11-16 19:57:37 +0100

    update client-server to be able to run

--- server.go
+++ server.go
@@ -1,149 +1,92 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"main/connect"
-	ra "main/ricart-agrawala"
+	"log"
+	"net"
 	"os"
+	"sync"
+
+	"github.com/Rasmus-Balthazar/Auction/auctionService"
+	"google.golang.org/grpc"
 )
 
+type ClientConnection struct {
+	stream auctionService.AuctionService_ConnectClient
+	pid    uint32
+	name   string
+}
+
 type Server struct {
-	time       uint64
-	pid        uint32
-	queue      chan ra.Request
-	replyQueue chan ra.Reply
-	state      ra.State
-	_time      uint64
-	connect.UnimplementedConnectServer
-	peers map[uint32]*Peer
+	auctionService.UnimplementedAuctionServiceServer
+	connections map[uint32]*ClientConnection
+	chBids      chan *auctionService.BidMessage
+	pid         uint32
+	result      auctionService.Outcome
 }
 
-func NewServer() *Server {
-	return &Server{
-		peers:      make(map[uint32]*Peer),
-		pid:        uint32(os.Getpid()),
-		queue:      make(chan ra.Request, 100), // Can't have more than 100 peers
-		replyQueue: make(chan ra.Reply, 100),
+type Auction struct {
+	highestBidder auctionService.Outcome
+	//State         auctionService.AuctionState_GOING
+	AuctionLocker sync.Mutex
+}
+
+var (
+	auctionLocker sync.Mutex
+
+	auction = auctionService.Outcome{
+		State:    auctionService.AuctionState_GOING,
+		Amount:   "",
+		BidderId: 0,
 	}
-}
+)
 
-// Called by grpc
-func (server *Server) SendMessage(ctx context.Context, msg *connect.Message) (*connect.Void, error) {
-	ra.Recv(server, msg)
+func server() {
+	// We need a listener for grpc
+	listener, err := net.Listen("tcp", net.JoinHostPort("localhost", *port))
+	if err != nil {
+		l.Fatalf("fail to listen on port %s: %v", *port, err)
+	}
+	defer listener.Close()
 
-	// Peers reply by sending a message with our pid
-	if server.GetPid() == msg.GetPid() {
-		l.Printf("got reply")
-		server.replyQueue <- ra.Reply{}
-	} else {
-		l.Printf("recv {%v}", msg)
-		ra.Receive(server, ra.NewRequest(msg))
+	server := &Server{
+		connections: make(map[uint32]*ClientConnection),
+		pid:         uint32(os.Getpid()),
 	}
 
-	return &connect.Void{}, nil
-}
+	// The usual gRPC server setup
+	go func() {
+		grpcServer := grpc.NewServer()
+		auctionService.RegisterAuctionServiceServer(grpcServer, server)
+		l.Printf("server %s is running on port %s", *name, *port)
+		if err := grpcServer.Serve(listener); err != nil {
+			l.Fatalf("stopped serving: %v", err)
+		}
+	}()
 
-// Called by grpc
-func (server *Server) JoinNetwork(ctx context.Context, peerJoin *connect.PeerJoin) (*connect.ConnectedTo, error) {
-	// Already connected to peer -> ignore
-	if _, ok := server.peers[peerJoin.GetPid()]; !ok {
-		client := server.ConnectClient(peerJoin.GetPort())
-		server.AddPeer(NewPeer(peerJoin.GetPid(), client))
-		l.Printf("connected to peer %d", peerJoin.GetPid())
-		fmt.Printf("New peer connected (total: %d)\n", len(server.peers))
+	// Recv messages and send them to everyone
+	for msg := range server.chBids {
+		l.Printf("send '%s'", msg)
 
-		// Tell the rest of the network about the new peer
-		for pid, peer := range server.peers {
-			var err error
-			if pid != peerJoin.GetPid() {
-				_, err = peer.client.JoinNetwork(ctx, peerJoin)
-			} else {
-				// Respond to the new peer telling them about us
-				_, err = peer.client.JoinNetwork(ctx, &connect.PeerJoin{
-					Pid:  server.GetPid(),
-					Port: *port,
-				})
-			}
+		// Adopt time from server in msg.
+		// Uncomment to make sure all messages always appear as the newest message.
+		// This might not be what we want though - if a message had latency, we might want to appear
+		// in the order from its original context.
+		//
+		// msg.time = server.time
 
-			if err != nil {
-				l.Fatalf("fail to propagate join: %v", err)
-			}
+		for _, client := range server.connections {
+			// Check if this message was randomly "lost"
+			log.Print(client.name)
+			//client.stream.Send(msg)
 		}
 	}
-
-	return &connect.ConnectedTo{Pid: server.GetPid()}, nil
 }
 
-func (server *Server) AddPeer(peer *Peer) {
-	server.peers[peer.pid] = peer
-}
+func (a *Auction) endAuction() {
+	a.AuctionLocker.Lock()
 
-// Unused because we don't want to implement leaving the network :)
-func (server *Server) RemovePeer(peer *Peer) {
-	delete(server.peers, peer.pid)
-}
+	defer a.AuctionLocker.Unlock()
 
-// Impl Lamport
-func (server *Server) GetPid() uint32 {
-	return server.pid
-}
-
-// Impl Lamport
-func (server *Server) GetTime() uint64 {
-	return server.time
-}
-
-// Impl LamportMut
-func (server *Server) SetTime(time uint64) {
-	// We need to know at what time we requested the lock.
-	// So don't update the time if we are currently requesting it.
-	if server.GetState() != ra.Wanted {
-		l.Printf("changing time %d -> %d", server.GetTime(), time)
-		server.time = time
-	}
-	// Save the time so we can update when we eventually get the lock.
-	server._time = time
-}
-
-// Impl RicartAgrawala
-func (server *Server) SetState(state ra.State) {
-	prev := server.state
-	l.Printf("changing state %s -> %s", server.state, state)
-	server.state = state
-	if prev == ra.Wanted {
-		server.SetTime(server._time)
-	}
-}
-
-// Impl RicartAgrawala
-func (server *Server) GetState() ra.State {
-	return server.state
-}
-
-// Impl RicartAgrawala
-func (server *Server) Multicast(req ra.Request) chan ra.Reply {
-	l.Printf("multicasting %v", req)
-	ra.Send(server)
-	req.SetTime(server.GetTime())
-
-	for _, p := range server.peers {
-		p.client.SendMessage(context.Background(), ToMessage(req))
-	}
-
-	return server.replyQueue
-}
-
-// Impl RicartAgrawala
-func (server *Server) Queue() chan ra.Request {
-	return server.queue
-}
-
-// Impl RicartAgrawala
-func (server *Server) Reply(req ra.Request) {
-	l.Printf("replying %v", req)
-	ra.Send(server)
-	req.SetTime(server.GetTime())
-	p := server.peers[req.GetPid()]
-	p.client.SendMessage(context.Background(), ToMessage(req))
+	//a.status = AuctionService.AuctionStatus_AUCTION_OVER
+	log.Printf("Auction ended - the winning bid was %v by %v\n!", a.Amount, highestBid.MadeBy)
 }
--- util.go
+++ util.go
@@ -1,30 +1,23 @@
 package main
 
 import (
-	"main/connect"
-	ra "main/ricart-agrawala"
 	"net"
 
+	"github.com/Rasmus-Balthazar/Auction/auctionService"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 // Dial grpc server listening on the given port
-func (server *Server) ConnectClient(port string) connect.ConnectClient {
+func (server *Server) ConnectAutionClient(port string) auctionService.AuctionServiceClient {
 	l.Printf("dialing %s", port)
+
 	conn, err := grpc.Dial(net.JoinHostPort("localhost", port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	if err != nil {
 		l.Fatalf("fail to dial: %v", err)
 	}
 
-	client := connect.NewConnectClient(conn)
-
+	client := auctionService.NewAuctionServiceClient(conn)
 	return client
-}
-
-func ToMessage(req ra.Request) *connect.Message {
-	return &connect.Message{
-		Time: req.GetTime(),
-		Pid:  req.GetPid(),
-	}
 }
